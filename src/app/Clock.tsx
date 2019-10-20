a6598a5209433f2f75924df3afec903f14d9c933
Mike Basdeo <mikebasdeo@gmail.com>
2019-10-20 15:03:10 -0400

    James Bondified the functional component example

--- src/app/Clock.tsx
+++ src/app/Clock.tsx
@@ -10,17 +10,18 @@ let time = observable({
 
 setInterval(() => {
     time.value = new Date()
-}, 1000)
+}, 1)
 
 export const Clock = observer(() => {
     const hours = time.value.getHours()
     const minutes = time.value.getMinutes()
     const seconds = time.value.getSeconds()
+    const milliseconds = time.value.getMilliseconds()
 
     return(
         <div>
             <h2>
-                {`${hours}:${minutes}:${seconds}`}
+                {`${hours}:${minutes}:${seconds}:${milliseconds}`}
             </h2>
         </div>
     )
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,20 +1,58 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+// class import
 import Sheet from './Sheet'
+// functional import
 import { Clock } from './Clock'
 
-const Hello: React.FunctionComponent<{ compiler: string, framework: string }> = (props) => {
+// functional typescript component
+const License: React.FunctionComponent<{licenseToKill: boolean}> = (props) => {
+  if (props.licenseToKill) {
+    return(
+      <div>
+        Access Granted
+      </div>
+    )
+  }
   return (
     <div>
-      <div>{props.compiler}</div>
-      <div>{props.framework}</div>
-      <Sheet />
-      <Clock />
+      Access Denied
     </div>
-  );
+  )
 }
 
+// practice functional typescript component
+const Person: React.FunctionComponent<{ name: string, isSpy: boolean, age: number}> = (props) => {
+  if (props.isSpy) {
+  return (
+    <div>
+      Welcome {props.name} <br/>
+      You are a spy
+      {props.children}
+    </div>
+    )
+  }
+  return (
+    <div>
+      Welcome {props.name} <br/>
+      You are not a spy
+      {props.children}
+    </div>
+  )
+}
+
+// main method
 ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
+  <div>
+    <Clock/>
+    <Person name='James Bond' isSpy={true} age={55}>
+      <License licenseToKill={true}/>
+    </Person>
+
+    <Person name='Q' isSpy={false} age={55}>
+      <License licenseToKill={false}/>
+    </Person>
+  </div>
+  ,
   document.getElementById("root")
 );
